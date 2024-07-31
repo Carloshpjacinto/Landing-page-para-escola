248c820589fa43275da96d4d8d97b1cf77260187
Carlos Henry Jr. <carlo.henrique37@gmail.com>
2024-07-31 00:24:04 -0300

    Subindo projeto

---
+++ index.js
@@ -0,0 +1,29 @@
+var setaDireita = window.document.getElementById("setaDireita")
+var setaEsquerda = window.document.getElementById("setaEsquerda")
+
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+
+function rolarDireita(){
+
+    leonardo.style = "display:none";
+
+    setaDireita.style = "display:none";
+
+    bruna.style = "display:flex";
+
+    setaEsquerda.style = "display:flex; margin-top:40px"
+
+}
+
+function rolarEsquerda(){
+
+    bruna.style = "display:none";
+
+    setaEsquerda.style = "display:none";
+
+    leonardo.style = "display:flex";
+
+    setaDireita.style = "display:flex;"
+}
